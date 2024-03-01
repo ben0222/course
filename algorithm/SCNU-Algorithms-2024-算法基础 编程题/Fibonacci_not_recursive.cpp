4fd32cf048937f265bfeb6e6e730b081bc59b319
ben0222 <20222131100@m.scnu.edu.cn>
2024-03-01 16:11:08 +0800

    对规划数组增加了int类型溢出处理

--- algorithm/SCNU-Algorithms-2024-算法基础 编程题/Fibonacci_not_recursive.cpp
+++ algorithm/SCNU-Algorithms-2024-算法基础 编程题/Fibonacci_not_recursive.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+// 一个特定的质数，常用来避免int类型溢出
+const int P = 998244353;
 int Fibonicci_not_recursive(int n)
 {
     vector<int> f(n + 1);
-    f[0] = 1;
     f[1] = 1;
-    for (int i = 2; i <= n; i++)
+    f[2] = 1;
+    for (int i = 3; i <= n; i++)
     {
-        f[i] = f[i - 1] + f[i - 2];
+        // f[i] = f[i - 1] + f[i - 2];
+        //  该数组可能出现int类型溢出，处理如下
+        f[i] = ((f[i - 1] + f[i - 2]) % P + P) % P;
     }
     return f[n];
 }
@@ -16,6 +20,6 @@ int main()
 {
     int n = 0;
     cin >> n;
-    cout << Fibonicci_not_recursive(n) % 998244353 << endl;
+    cout << Fibonicci_not_recursive(n) << endl;
     return 0;
 }
