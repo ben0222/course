d4c229832aabf9915d20d387ec7670518fff3e16
ben0222 <20222131100@m.scnu.edu.cn>
2024-03-31 15:52:34 +0800

    bfs done

--- lanqiao/2023B/code/3517.cpp
+++ lanqiao/2023B/code/3517.cpp
@@ -5,15 +5,29 @@ using namespace std;
 const int N = 1e5 + 10;
 typedef pair<int, int> p;
 
-vector<int> edge[N]; // 图存储：存这个点的邻接点
+vector<int> edge[N]; // 图存储：存这个点的邻接点,为每个点开辟一个vector
 int n, m;
 int weight[N];  // 存边的权值
 map<p, int> id; // 存边的编号
 
-bool dfs(int s, int u, int father, int v)
+bool dfs(int s, int u, int father, int v) // 起点s，终点v，当前点u，当前点的父节点father
 {
     if (u == v)
         return true;
+
+    for (int i = 0; i < edge[u].size(); i++) // 遍历当前点u的所有子节点
+    {
+        int son = edge[u][i];
+        if (father == son)
+            continue;
+        if (dfs(s, son, u, v))
+        {
+            int ID = id[{u, son}];
+            weight[ID]++;
+            return true;
+        }
+    }
+    return false;
 }
 void solve()
 {
