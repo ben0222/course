2fd417cbf0ee13e350e042beb3fa35170c03e07a
ben0222 <20222131100@m.scnu.edu.cn>
2024-04-08 15:06:59 +0800

    🐞 fix:
    应该遍历字符串而非bool数组，所以边界判断也用字符串的size()

--- lanqiao/2023C/3519.cpp
+++ lanqiao/2023C/3519.cpp
@@ -15,32 +15,32 @@ void solve()
     vector<bool> st(str.size()); // 记录i位置是否匹配过
     for (int i = 0; i < str.size(); i++)
     {
-        if (st[i] != '?')
+        if (str[i] != '?')
         {
             if (st[i] == false)
             {
-                if (st[i - 1] == st[i] && i - 1 >= 0 && st[i - 1] == false)
+                if (str[i - 1] == str[i] && i - 1 >= 0 && st[i - 1] == false)
                 {
                     st[i - 1] = st[i] = true;
                     count++;
                 }
-                else if (st[i] == st[i + 1] && i + 1 < st.size() && st[i + 1] == false)
+                else if (str[i] == str[i + 1] && i + 1 < str.size() && st[i + 1] == false)
                 {
                     st[i] = st[i + 1] = true;
                     count++;
                 }
             }
         }
-        else
+        else // 如果当前字符是问号，则检查其相邻字符是否已经被匹配过，如果没有，则将当前字符和相邻字符标记为匹配，并增加答案计数。
         {
             if (st[i] == true)
-                continue; // 已经被配对了，下一个
-            if (st[i - 1] == true && i - 1 >= 0)
+                continue; // st[i]经历了下面的操作后会配对成功，则遍历到st[i+1]时候，需要识别到st[i]是识别过的。
+            if (st[i - 1] == false && i - 1 >= 0)
             {
                 st[i - 1] = st[i] = true;
                 count++;
             }
-            else if (st[i + 1] == true && i + 1 < str.size())
+            else if (st[i + 1] == false && i + 1 < str.size())
             {
                 st[i] = st[i + 1] = true;
                 count++;
@@ -49,7 +49,7 @@ void solve()
     }
     cout << count << endl;
 }
-unsigned main()
+signed main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -57,4 +57,5 @@ unsigned main()
     int t = 1;
     while (t--)
         solve();
+    return 0;
 }
