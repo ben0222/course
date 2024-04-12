fc64abde6c8441b0ee50d2b1b2c22d839668e423
ben0222 <20222131100@m.scnu.edu.cn>
2024-04-12 21:25:03 +0800

    飞机降落：bfs

--- lanqiao/2023B/code/3511.cpp
+++ lanqiao/2023B/code/3511.cpp
@@ -59,11 +59,11 @@ void solve()
 }
 int main()
 {
-    // ios::sync_with_stdio(0);
-    // cin.tie(0);
-    // int t = 1;
-    // cin >> t;
-    // while (t--)
-    solve();
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
     return 0;
 }
